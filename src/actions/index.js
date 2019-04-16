a1727bae1bb6bc3a1e0db8130a4e4f8a15956077
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-15 17:40:59 -0700

    Updated action suite for Posts.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -65,6 +65,27 @@ export const getUsers = () => dispatch => {
     );
 };
 
+// posts action suite for Posts.js
+
+export const GET_POSTS_START = "GET_POSTS_START";
+export const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
+export const GET_POSTS_FAIL = "GET_POSTS_FAIL";
+
+export const getPosts = () => dispatch => {
+  dispatch({ type: GET_POSTS_START });
+  axiosWithAuth()
+    .get("https://expat-backend.herokuapp.com/posts/0")
+    .then(res => {
+      console.log("GOT POSTS DATA!", res.data);
+      // DOUBLE CHECK PAYLOAD
+      dispatch({ type: GET_POSTS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: GET_POSTS_FAIL, payload: err.response });
+    });
+};
+
+
 // NewPost action creator PostForm.js
 export const NEW_POST_START = "NEW_POST_START";
 export const NEW_POST_SUCCESS = "NEW_POST_SUCCESS";
